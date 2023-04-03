40d740b6762f2022bea90c565a867b52ed0a9db0
Rayane Garcia <112990445+rayanegarcia@users.noreply.github.com>
2023-04-03 19:39:55 -0300

    Repetições

---
+++ aula#14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+var c = 1                   |
+do {                        |
+    console.log(c)          |
+    c++                     |
+} while (c <= 5)            V
+*/
+
+// ------------------ //
+
+console.log('Vai começar...')
+for ( var c = 1 ; c <= 5 ; c++ ) {
+    console.log(c)
+}
+console.log('FIM!')
